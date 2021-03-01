2ee30940c3785a9f8617a1b7502998bc7e669071
anita2498 <anitaputhenpurayil@gmail.com>
2021-03-01 18:54:46 -0500

    project done

--- create_table.js
+++ create_table.js
@@ -9,13 +9,7 @@ class Deal{
 }
 var currentDealId =0;
 var myData = null;
-// myData = [new Deal(0,"Microsoft","Apollo Project","Mary",1000),
-//         new Deal(1,"Intel","Hermes Project","Bob",10000),
-//         new Deal(2,"Apple","Zeus Project","Jane",100000)
-//         ]
 
-//         currentDealId = myData.length;
-//         localStorage.setItem("myData", JSON.stringify(myData));
 function initialize(){
     if (localStorage.getItem("myData") ===null){
         //alert("inside if")
@@ -42,11 +36,6 @@ function initialize(){
 // localstorage allows us to persist key value pairs in a way that would survive page refreshes, navigation, and user closing/reopening browser.
 // localstorage has limits to the size of each object stored.   
 
-// localStorage.setItem("myData", "test")
-
-// var myDataTest = localStorage.getItem("myData")
-
-
 
 function CreateTableFromJSON() {    
     initialize();
@@ -68,51 +57,6 @@ function CreateTableFromJSON() {
     })
 }
 
-//     // EXTRACT VALUE FOR HTML HEADER. 
-//     // ('Deal ID', 'Deal Name', 'Category' and 'Price')
-//     var col = [];
-//     for (var i = 0; i < myData.length; i++) {
-//         for (var key in myData[i]) {
-//             if (col.indexOf(key) === -1) {
-//                 col.push(key);
-//             }
-//         }
-//     }
-
-//     // CREATE DYNAMIC TABLE.
-//     var table = document.createElement("table");
-
-//     // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-
-//     var tr = table.insertRow(-1);                   // TABLE ROW.
-
-//     for (var i = 0; i < col.length; i++) {
-//         var th = document.createElement("th");      // TABLE HEADER.
-//         th.innerHTML = col[i];
-//         tr.appendChild(th);
-//     }
-
-//     // ADD JSON DATA TO THE TABLE AS ROWS.
-//     for (var i = 0; i < myData.length; i++) {
-
-//         tr = table.insertRow(-1);
-
-//         for (var j = 0; j < col.length; j++) {
-//             var tabCell = tr.insertCell(-1);
-//             tabCell.innerHTML = myData[i][col[j]];
-//         }
-//         // Insert Extra Cell for the Delete Icon
-//         //TODO: Complete this
-//         //var tabCell = tr.insertCell(-1);
-//         //tabCell.innerHTML = '<button onclick="DeleteRow(' + myData[i].dealId + ')"> <img src="trashcan.png"> </button>'
-
-//     }
-
-//     // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-//     var divContainer = document.getElementById("showData");
-//     divContainer.innerHTML = "";
-//     divContainer.appendChild(table);
-// }
 
 function AddNewDeal() {
     var clientName = document.getElementById("clientNameInput").value;
@@ -133,7 +77,6 @@ function AddNewDeal() {
 }
 
 function InsertRow(dealId, clientName, projectName, projectManager, projectCost) {
-   // myData.push({"dealId": dealId, "client_name" : clientName, "project_name" : projectName, "project_manager" : projectManager, "project_cost" : projectCost})
     var a= new Deal(dealId, clientName, projectName, projectManager, projectCost);
     myData.push(a);
     currentDealId++;
